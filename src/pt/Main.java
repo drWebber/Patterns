cbddb2230610c833f9a16012be497b7b0bf5d149
Maxim Fedorenko <m.fedorenko@tut.by>
2017-12-04 16:05:43 +0200

    builder (account)

--- src/pt/Main.java
+++ src/pt/Main.java
@@ -1,5 +1,7 @@
 package pt;
 
+import java.util.Date;
+import pt.builder.account.Account;
 import pt.builder.house.*;
 
 public class Main {
@@ -7,5 +9,13 @@ public class Main {
         HouseDirector hDirector = new HouseDirector(new StoneHouseBuilder());
         hDirector.buildHouse();
         System.out.println(hDirector.getHouse());
+        
+        Account account = Account.getBuilder()
+                .setUsername("Vasiliy")
+                .setPassword("123")
+                .setBio("butcher")
+                .setDob(new Date())
+                .build();
+        System.out.println(account);
     }
 }
---
+++ src/pt/builder/account/Account.java
@@ -0,0 +1,48 @@
+package pt.builder.account;
+
+import java.util.Date;
+
+public class Account {
+    private String username;
+    private String password;
+    private String bio;
+    private Date dob;
+
+    public Account() { }
+
+    public static Builder getBuilder() {
+        return new Account().new Builder();
+    }
+    
+    @Override
+    public String toString() {
+        return "Account [username=" + username + ", password=" + password
+                + ", bio=" + bio + ", dob=" + dob + "]";
+    }
+    
+    public class Builder {
+        public Builder setUsername(String username) {
+            Account.this.username = username;
+            return this;
+        }
+        
+        public Builder setPassword(String password) {
+            Account.this.password = password;
+            return this;
+        }
+        
+        public Builder setBio(String bio) {
+            Account.this.bio = bio;
+            return this;
+        }
+        
+        public Builder setDob(Date dob) {
+            Account.this.dob = dob;
+            return this;
+        }
+        
+        public Account build() {
+            return Account.this;
+        }
+    }
+}
